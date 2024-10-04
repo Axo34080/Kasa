01b62b4732adab3f59009a46164b6ee38548c7b4
Marc <158296221+Axo34080@users.noreply.github.com>
2024-10-04 23:30:27 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+import App from './App';
 import "./style/main.scss";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
